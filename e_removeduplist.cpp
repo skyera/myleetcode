ca736a7b175d4fb70037b12e5a2cd72b747cd946
skyera <zgliu71@gmail.com>
2017-08-31 21:25:30 -0700

    remove dup from slist-recursive

--- e_removeduplist.cpp
+++ e_removeduplist.cpp
@@ -1,3 +1,4 @@
+// remove duplicates from sorted list
 // 8/31/2017
 // Easy
 #include <iostream>
@@ -24,19 +25,60 @@ ListNode* removedups(ListNode *head) {
     return head;
 }
 
+void recur(ListNode *prev, ListNode *cur) {
+    if (!cur) return;
+
+    if (prev->val == cur->val) {
+        prev->next = cur->next;
+        delete cur;
+        recur(prev, prev->next);
+    } else {
+        recur(cur, cur->next);
+    }
+}
+
+// recursive
+ListNode *removedup2(ListNode *head) {
+    if (!head) return NULL;
+
+    ListNode dummy(head->val + 1);
+    dummy.next = head;
+
+    recur(&dummy, head);
+
+    return dummy.next;
+}
+
 int main()
 {
-    ListNode *n1 = new ListNode(1);
-    ListNode *n2 = new ListNode(1);
-    ListNode *n3 = new ListNode(2);
-    n1->next = n2;
-    n2->next = n3;
+    {
+        ListNode *n1 = new ListNode(1);
+        ListNode *n2 = new ListNode(1);
+        ListNode *n3 = new ListNode(2);
+        n1->next = n2;
+        n2->next = n3;
 
-    ListNode *head = removedups(n1);
+        ListNode *head = removedups(n1);
 
-    for (ListNode *n = head; n; n = n->next)
-        cout << n->val << " ";
-    
-    cout << endl;
+        for (ListNode *n = head; n; n = n->next)
+            cout << n->val << " ";
+
+        cout << endl;
+    }
+
+    {
+        ListNode *n1 = new ListNode(1);
+        ListNode *n2 = new ListNode(1);
+        ListNode *n3 = new ListNode(2);
+        n1->next = n2;
+        n2->next = n3;
+
+        ListNode *head = removedup2(n1);
+
+        for (ListNode *n = head; n; n = n->next)
+            cout << n->val << " ";
+
+        cout << endl;
+    }
     return 0;
 }
