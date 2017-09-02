0ddf3da7e5a96d32b289826ea972600a0af35d25
skyera <zgliu71@gmail.com>
2017-09-02 16:44:08 -0700

    length of last word 4

--- e_lengthlastword.cpp
+++ e_lengthlastword.cpp
@@ -36,11 +36,48 @@ int lengthoflastword2(const string &s) {
     return end - i;
 }
 
+int lengthoflastword3(const char *s) {
+    int len = 0;
+
+    while (*s) {
+        if (*s++ != ' ')
+            ++len;
+        else if (*s && *s != ' ') {
+            len = 0;
+        }
+    }
+    return len;
+}
+
+int lengthoflastword4(const char *s) {
+    int len = 0;
+
+    while (*s) {
+        if (*s != ' ') {
+            ++len;
+        }
+        else {// is space
+            len = 0;
+        }
+        ++s;
+    }
+
+    return len;
+}
+
+
 int main()
 {
     string s("Hello World ");
 
     int len = lengthoflastword(s);
+    assert(len == 5);
+
+    len = lengthoflastword3(s.c_str());
+    assert(len == 5);
+    
+    char s1[] = "hello world";
+    len = lengthoflastword4(s1);
     assert(len == 5);
 
     len = lengthoflastword2(s);
