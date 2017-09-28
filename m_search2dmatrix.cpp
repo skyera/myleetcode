ab1b87224ee66835c4f4af3082f8acd5e45d5067
skyera <zgliu71@gmail.com>
2017-09-28 08:21:58 -0700

    search 2d matrix

---
+++ m_search2dmatrix.cpp
@@ -0,0 +1,34 @@
+// Search a 2D matrix
+// Medium
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool searchmatrix(const vector<vector<int> > &matrix, int target) {
+    if (matrix.empty()) return false;
+    const size_t m = matrix.size();
+    const size_t n = matrix.front().size();
+
+    int first = 0;
+    int last = m * n;
+
+    while (first < last) {
+        int mid = first + (last - first) / 2;
+        int value = matrix[mid/n][mid%n];
+
+        if (value == target)
+            return true;
+        else if (value < target)
+            first = mid + 1;
+        else
+            last = mid;
+    }
+
+    return false;
+}
+
+int main()
+{
+    return 0;
+}
